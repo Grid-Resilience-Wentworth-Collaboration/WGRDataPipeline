9d77e6c0ab9370fea12ee8264bc7bc1d89365c5c
Prakash Manandhar <jsaaymi@gmail.com>
2024-06-28 05:03:29 -1000

    Working on DataAvailability

--- src/RunDataAvailability.py
+++ src/RunDataAvailability.py
@@ -1,2 +1,13 @@
 from WGRDataPipelines.Pipeline1Forcast import DataAvailability
-print(DataAvailability().getDataGaps())
+from dateutil import parser
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+NodeId = os.getenv("SERIES_CREATOR_NODE_ID")
+START = parser.parse(os.getenv("SERIES_CREATOR_START_DATETIME"))
+HOURS = int(os.getenv("SERIES_CREATOR_HISTORY_HOURS"))
+TOL = int(os.getenv("SERIES_CREATOR_TOLERANCE_MINUTES"))
+
+dataGapsChecker = DataAvailability(NodeId, START, TOL, HOURS)
+print(dataGapsChecker.getDataGaps())
